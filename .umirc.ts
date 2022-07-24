c24d86f7e28076e161a5412d89105941e4345af1
lpb <462570646@qq.com>
2022-07-24 19:12:38 +0800

    formily initialize

---
+++ .umirc.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', component: '@/pages/index' },
+  ],
+  fastRefresh: {},
+  antd:{},
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      { libraryName: 'antd', libraryDirectory: 'es', style: true },
+      'antd',
+    ],
+    [
+      'babel-plugin-import',
+      { libraryName: '@formily/antd', libraryDirectory: 'esm', style: true },
+      '@formily/antd',
+    ],
+  ],
+});
